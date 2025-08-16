6192c13234ae9996b2e9a6b64a9570c525bc3aca
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-08-16 10:06:46 +0700

    feat: define routes constant

---
+++ src/constants/routes.ts
@@ -0,0 +1,17 @@
+export const authenticatedRoutes = [
+  {
+    title: "Dashboard",
+    path: "/dashboard"
+  }
+]
+
+export const unauthenticatedRoutes = [
+  {
+    title: "Sign in",
+    path: "/signin"
+  },
+  {
+    title: "Sign up",
+    path: "/signup"
+  },
+]
