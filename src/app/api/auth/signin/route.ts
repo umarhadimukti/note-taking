e289826e1f66c9ed6916075d9fab3dcae5cf5f34
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-08-16 17:04:20 +0700

    feat(auth): complete login api

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,35 @@
+import { IUserSignin } from "@/types";
+import { Prisma } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { authenticateUser } from "@/lib/auth";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password }: IUserSignin = await request.json();
+
+    const user = await authenticateUser(email, password);
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    return NextResponse
+      .json({ message: "Sign in successul", data: user })
+      .cookies.set("user-session", JSON.stringify(user), {
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 60 * 60 * 24 * 7 // 1 minggu
+      });
+  } catch (err) {
+    if (err instanceof Error) {
+      return NextResponse.json(
+        { error: err.message || "Bad Request" },
+        { status: 400 }
+      )
+    }
+    return NextResponse.json(
+      { error: "Login failed" },
+      { status: 500 }
+    );
+  }
+}
